4f7f250b3f9c1d69c3ec36410fc899df7f965b0b
Ben Heidemann <ben@heidemann.co.uk>
2024-01-08 22:45:50 +0000

    formatting

--- packages/components-blog/index.ts
+++ packages/components-blog/index.ts
@@ -1,8 +1,6 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
-import BlogExample from './src/BlogExample.astro';
+import BlogExample from "./src/BlogExample.astro";
 
-export {
-  BlogExample,
-};
+export { BlogExample };
