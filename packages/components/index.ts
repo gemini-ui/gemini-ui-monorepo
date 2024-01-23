e50895c9fd2b0ebf9292d4a88f9eb1cd84e226f8
Ben Heidemann <56122437+bcheidemann@users.noreply.github.com>
2024-01-23 22:19:15 +0000

    feat(components): Add image component (#25)
    * feat(components): add image component
    
    * feat(website): add image component docs
    
    * fix(website): add alt for images
    
    * fix(website): add aspect ratio to images to reduce layout shift

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,12 +1,13 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
+import Alert from "./src/Alert.astro";
 import AnchorButton from "./src/AnchorButton.astro";
 import Button from "./src/Button.astro";
 import Card from "./src/Card.astro";
+import Divider from "./src/Divider.astro";
 import DynamicGlow from "./src/DynamicGlow.astro";
 import Grid from "./src/Grid.astro";
-import Divider from "./src/Divider.astro";
-import Alert from "./src/Alert.astro";
+import Image from "./src/Image.astro";
 
-export { Alert, AnchorButton, Button, Card, Divider, DynamicGlow, Grid };
+export { Alert, AnchorButton, Button, Card, Divider, DynamicGlow, Grid, Image };
