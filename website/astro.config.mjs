b38aa0877b7fb14ae45dbb4362f7affebedbfcbf
Chris Jenkins <ichrisjjenkins@gmail.com>
2024-03-12 21:52:25 +0200

    feat(components): Starlight docs init

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,4 +1,27 @@
 import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Gemini UI Docs",
+      social: {
+        github: "https://github.com/gemini-ui/gemini-ui-monorepo",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Typography", link: "/guides/typography" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
