3ff5773d08130af92722e9eeefa7f66ad92e3ea9
Chris Jenkins <ichrisjjenkins@gmail.com>
2024-03-17 07:20:27 +0000

    feat(website): Added starlight docs for website, rebuilt Grid using flexbox (#47)
    * feat(components): Starlight docs init
    
    * fix(components): add default variant
    
    * feat(website): Added initial docs for most components
    
    * feat(components): Rebuilt Grid component using flexbox, added to docs
    
    * feat(website): PR feedback, added warning banner to docs
    
    ---------
    
    Co-authored-by: Ben Heidemann <ben@heidemann.co.uk>

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,4 +1,27 @@
 import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import liveCode from "astro-live-code";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    liveCode(),
+    starlight({
+      customCss: ["./src/styles/global.css"],
+      title: "Gemini UI Docs",
+      social: {
+        github: "https://github.com/gemini-ui/gemini-ui-monorepo",
+        "x.com": "https://twitter.com/GeminiUiDotDev",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "Components",
+          autogenerate: { directory: "components" },
+        },
+      ],
+    }),
+  ],
+});
